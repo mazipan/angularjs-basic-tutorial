38cd06bce8643d10310fa9de6ff52bfda46d3448
Albert JT <albertjulian97@gmail.com>
2017-03-15 09:00:19 +0700

    Added another get

--- albertjt/AppController.js
+++ albertjt/AppController.js
@@ -5,8 +5,44 @@ c.controller('CoreController', function ($scope, $http){
     $scope.fullName = $scope.firstName + " " + $scope.lastName;
   }
 
-  $scope.getData = function () {
+  $scope.getPostData = function () {
     $http.get("https://jsonplaceholder.typicode.com/posts")
+    .then( function(response) {
+      // console.log(response.data);
+      // console.log(sharedObject);
+      $scope.data = response.data;
+    });
+  }
+
+  $scope.getCommentData = function () {
+    $http.get("https://jsonplaceholder.typicode.com/comments")
+    .then( function(response) {
+      // console.log(response.data);
+      // console.log(sharedObject);
+      $scope.data = response.data;
+    });
+  }
+
+  $scope.getAlbumData = function () {
+    $http.get("https://jsonplaceholder.typicode.com/albums")
+    .then( function(response) {
+      // console.log(response.data);
+      // console.log(sharedObject);
+      $scope.data = response.data;
+    });
+  }
+
+  $scope.getToDoData = function () {
+    $http.get("https://jsonplaceholder.typicode.com/todos")
+    .then( function(response) {
+      // console.log(response.data);
+      // console.log(sharedObject);
+      $scope.data = response.data;
+    });
+  }
+
+  $scope.getUserData = function () {
+    $http.get("https://jsonplaceholder.typicode.com/users")
     .then( function(response) {
       // console.log(response.data);
       // console.log(sharedObject);
